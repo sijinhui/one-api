1fc9f1fbde94198b70f6f10f3ed2df33bdac33b5
sijinhui <sijinhui@qq.com>
2025-02-16 00:47:28 +0800

    透传流式响应

--- relay/adaptor/common.go
+++ relay/adaptor/common.go
@@ -3,6 +3,7 @@ package adaptor
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"github.com/gin-gonic/gin"
 	"github.com/songquanpeng/one-api/common/client"
 	"github.com/songquanpeng/one-api/relay/meta"
@@ -46,6 +47,26 @@ func DoRequest(c *gin.Context, req *http.Request) (*http.Response, error) {
 	if resp == nil {
 		return nil, errors.New("resp is nil")
 	}
+    contentType := resp.Header.Get("Content-Type")
+    if strings.HasPrefix(contentType, "text/event-stream") {
+        // [1] 透传流响应头
+        c.Writer.Header().Del("Content-Length")
+        c.Writer.Header().Set("Content-Type", "text/event-stream")
+        for k, v := range resp.Header {
+            c.Writer.Header().Set(k, strings.Join(v, ", "))
+        }
+        c.Writer.WriteHeader(resp.StatusCode)
+
+        // [2] 流式拷贝数据到客户端
+        defer resp.Body.Close()
+        _, copyErr := io.Copy(c.Writer, resp.Body)
+
+        // 返回空的响应体（因其已被透传）
+        return &http.Response{
+            StatusCode: resp.StatusCode,
+            Header:     resp.Header,
+        }, copyErr
+    }
 	_ = req.Body.Close()
 	_ = c.Request.Body.Close()
 	return resp, nil
